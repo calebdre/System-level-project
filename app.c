ba10e72b34248f2e3786e390477e922f5eb6dba0
Caleb Lewis <caleb_dre@yahoo.com>
2017-04-20 19:57:08 -0400

    added section comments

--- app.c
+++ app.c
@@ -1,6 +1,56 @@
 #include <stdio.h>
 
+/****
+ * 
+ * function for basic file handing. Everyone should use this if they need to access the file
+ *
+ ****/
+
+
+
+
+
+
+
+
+
+/****
+ *
+ * function(s) that search file for row or rows
+ *
+****/
+
+
+
+
+
+
+
+
+/****
+ *
+ * function(s) to make additional rows or delete rows
+ *
+****/
+
+
+
+
+
+
+
+/****
+ *
+ * function(s) to edit rows
+ *
+ ****/ 
+
+
+
+
+
 int main(void) {
+    // logic for user input and responding to it goes here
     printf("Hello world!\n");
     return 0;
 }
