7b5852bc9d5634f0e893ffbc0ea615817b07b7af
Caleb Lewis <caleb_dre@yahoo.com>
2017-04-23 18:05:52 -0400

    added save books function

--- app.c
+++ app.c
@@ -11,7 +11,7 @@ typedef struct Book {
     char *dueDate;
 } Book;
 
-#define EMPTY_BOOK(X) Book X = { .id = 0, .name = "none", .author = "none", .possession = "none", .checkedOutAt = "none", .dueDate = "none" };
+#define EMPTY_BOOK(X) Book X = { .id = "0", .name = "none", .author = "none", .possession = "none", .checkedOutAt = "none", .dueDate = "none" };
 
 Book* getBooks() {
     FILE *inputfile;
@@ -61,46 +61,25 @@ Book* getBooks() {
 	return allBooks;
 }
 
-void saveBooks(Book *books) {
+void saveBooks(Book *books, int booksArraySize) {
+	FILE *inputfile;
+	inputfile = fopen("library.csv", "w+");
+	int i;
+	for (i = 0; i < booksArraySize; i++){
+		fprintf(inputfile, "%s,%s,%s,%s,%s,%s\n", 
+			books[i].id,
+			books[i].name,
+			books[i].author,
+			books[i].possession,
+			books[i].checkedOutAt,
+			books[i].dueDate);
+	}
 
+	fclose(inputfile);
 }
 
-
-
-/****
- *
- * function(s) that search file for row or rows
- *
-****/
-
-
-
-
-
-
-
-
-/****
- *
- * function(s) to make additional rows or delete rows
- *
-****/
-
-
-
-
-
-
-
-/****
- *
- * function(s) to edit rows
- *
- ****/ 
-
-
 void addBook() {
-	print("add book");
+	printf("add a book");
 }
 
 void deleteBook() {
@@ -165,22 +144,22 @@ int main(void) {
     char input;
 
     while(1) {
-	printf("\n\nWhat would you like to do?\n");
-	printf("(a) add a book \n(d) delete a book \n(o) check out a book\n(r) return a book\n(s) check the status of a book\n(q) view books written by an author\n(u) view books checked out by a given user.\n(x) quit\n\n");
-	
-	while ((input = getchar()) != '\n' && input != EOF) { 
-	    if (input == 'x') {
-		goto end;
-	    }
+		printf("\n\nWhat would you like to do?\n");
+		printf("(a) add a book \n(d) delete a book \n(o) check out a book\n(r) return a book\n(s) check the status of a book\n(q) view books written by an author\n(u) view books checked out by a given user.\n(x) quit\n\n");
+		
+		while ((input = getchar()) != '\n' && input != EOF) { 
+			if (input == 'x') {
+				goto end;
+			}
 
-	    int i;
-	    for (i = 0; i < sizeof(allowedCharacters) / sizeof(allowedCharacters[0]); i++){
-		if (allowedCharacters[i] == input) {
-		    executeAction(input);
-		    break;
+			int i;
+			for (i = 0; i < sizeof(allowedCharacters) / sizeof(allowedCharacters[0]); i++){
+				if (allowedCharacters[i] == input) {
+					executeAction(input);
+					break;
+				}
+			}		
 		}
-	    }			
-	}
     };
     
     end:
